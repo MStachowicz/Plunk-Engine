7c96d0c544be9e329c0c99f5dff014dfd02981ee
Michal <michal_1995@hotmail.co.uk>
2018-04-28 17:13:12 +0100

    Added the component interface.

---
+++ Plunk Engine/IComponent.h
@@ -0,0 +1,30 @@
+#ifndef ICOMPONENT_H
+#define ICOMPONENT_H
+
+class IComponent
+{
+public:
+	enum ComponentFlags {								// BITS								// VAL			
+		COMPONENT_NONE = 0,								// 0000 0000 0000 0000 0000			0
+		COMPONENT_RIGID_BODY = 1 << 1,					// 0000 0000 0000 0000 0010			2
+		COMPONENT_COLLIDABLE = 1 << 2,					// 0000 0000 0000 0000 0100			4	
+		COMPONENT_DIRECTIONAL_LIGHT = 1 << 3,			// 0000 0000 0000 0000 1000			8
+		COMPONENT_SPOTLIGHT = 1 << 4,					// 0000 0000 0000 0001 0000			16
+		COMPONENT_POINTLIGHT = 1 << 5,					// 0000 0000 0000 0010 0000			32
+		COMPONENT_SHADOW_CASTER = 1 << 6,				// 0000 0000 0000 0100 0000			64
+	};
+
+	IComponent(ComponentFlags componentType) : componentType(componentType)
+	{}
+	~IComponent()
+	{}
+
+	ComponentFlags GetComponentType()
+	{
+		return componentType;
+	}
+
+private:
+	ComponentFlags componentType;
+};
+#endif
